3f28b1d0594af6479ae2d548d6886520c637b78a
huangguodong <286832035@qq.com>
2020-09-21 18:01:31 +0800

    spring-security放行不通过原因提交--conroller层注解只有@Controller 所以找页面去了，所以返回404  真的菜

--- stock-admin/src/main/java/com/seek/stocks/controller/UmsAdminController.java
+++ stock-admin/src/main/java/com/seek/stocks/controller/UmsAdminController.java
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller(value = "/admin")
+@RestController
+@RequestMapping("/admin")
 public class UmsAdminController {
 
     @Autowired
